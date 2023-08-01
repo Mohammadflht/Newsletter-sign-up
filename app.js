1171fa2fae520b806a2fb2b6bb8b24bbcb8e4bd8
Mohammad <mfalahat2002@gmail.com>
2023-08-01 14:06:29 +0330

    declare Success container in JavaScript

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const formPreview = document.querySelector(".container");
 const emailValue = document.querySelector("input");
-const button = document.querySelector("button");
+const confirmBtn = document.querySelector("button");
+const dismissBtn = document.querySelector("#dismiss-btn");
 const emailError = document.querySelector((".email-error"));
+const validUserEmail = document.querySelector('#valid-email')
+const successPart = document.querySelector(".success-part");
 const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 
-button.addEventListener('click', () => {
+confirmBtn.addEventListener('click', () => {
     if (emailValue.value === ''){
         emailError.innerHTML = "Enter your email";
         emailError.style.color ="hsl(4, 100%, 67%)";
     }
     else if (emailValue.value.match(regex)) {
         formPreview.style.display = "none";
+        successPart.style.display = "block";
+        validUserEmail.innerHTML = emailValue.value;
     }
     else {
         emailError.innerHTML = "Valid email reauired";
@@ -19,7 +24,12 @@ button.addEventListener('click', () => {
         emailValue.style.borderColor = "hsla(4, 100%, 67%)"
         emailValue.style.color = "hsla(4, 100%, 67%)"
     }
-})
+});
+
+dismissBtn.addEventListener("click", () => {
+    formPreview.style.display = "flex";
+    successPart.style.display = "none";
+});
 
 
 
